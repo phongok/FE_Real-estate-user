89643c8bd5a9c276159297ad30e320285c6d92a4
phongok <phongdevgithub666666@gmail.com>
2023-04-30 21:58:43 +0700

    update report user

--- src/Components/Detail_Home/Detail.jsx
+++ src/Components/Detail_Home/Detail.jsx
@@ -2,138 +2,262 @@ import React, { useEffect, useState } from 'react';
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import {HiOutlineLocationMarker} from 'react-icons/hi'
+import { HiOutlineLocationMarker } from 'react-icons/hi'
 import Button from '@mui/material/Button';
-import axios from "axios" 
+import axios from "axios"
 
 import './detailhome.css'
 import { useParams } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+function Detail() {
 
-function Detail  () {
-
-    const {id} = useParams()
+    const { id } = useParams()
     const [dataDetail, setDataDetail] = useState()
-    useEffect(()=>{
-    console.log(id)
-    let config = {
-      method: 'get',
-      maxBodyLength: Infinity,
-      url: `http://localhost:8081/api/realestates/${id}`,
-      headers: { }
-    };
-    
-    axios.request(config)
-    .then((response) => {
-        if(response?.status === 200){
-            setDataDetail(response?.data)
-        }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-},[])
 
 
-        return ( <div className="container">
-                    <div className='real-estate-infor'>
-                    <div className="flex">
-                        
-                        <div className="carousel-image-real-estate">
 
-                           <Carousel>
-                               <div>
-                                   <img src={dataDetail?.url_img1} alt='' />
-                                   {/* <p className="legend">Ảnh 1</p> */}
-                               </div>
-                               <div>
-                                   <img src={dataDetail?.url_img2} alt='' />
-                                   {/* <p className="legend">Ảnh 2</p> */}
-                               </div>
-                               <div>
-                                   <img src={dataDetail?.url_img3} alt='' />
-                                   {/* <p className="legend">Ảnh 3</p> */}
-                               </div>
-                               <div>
-                                   <img src={dataDetail?.url_img4} alt='' />
-                                   {/* <p className="legend">Ảnh 4</p> */}
-                               </div>
-                               <div>
-                                   <img src={dataDetail?.url_img5} alt='' />
-                                   {/* <p className="legend">Ảnh 5</p> */}
-                               </div>
-                               <div>
-                                   <img src={dataDetail?.url_img6} alt='' />
-                                   {/* <p className="legend">Ảnh 6</p> */}
-                               </div>
-                       </Carousel>
-                       </div>
-                       <div className="detail-real-estate">
-                           <img alt="Remy Sharp" src={dataDetail?.user.url} style={{width:150, height:150, textAlign:'center', borderRadius:100, border: '2px solid black'}}/>
-                           <br />
-                           <br />
-                           <h5 style={{textAlign:'center'}}>Được đăng bởi</h5>
-                           <p style={{fontSize:30, fontWeight:'bold', color:'black', textAlign:'center'}}>{dataDetail?.user.name}</p>
-                           <a href={`/real-state-user/${dataDetail?.user.id}`}>
-                           <Button variant="outlined">Xem các sản phẩm liên quan</Button> <br />
-                           </a>
-                           <br />
-                           <ul>
-                               <li>SDT: {dataDetail?.user.phone}</li>
-                               <li>Email: {dataDetail?.user.username}</li>
-                           </ul>
-                           <Button variant="outlined">Yêu cầu liên lạc lại</Button> <br />
-                           
-                       </div>
-               </div>
-               <div>
-                   <h2 style={{color:'black'}}>{dataDetail?.name}</h2>
-               </div>
-               <div className='flex'>
-                   <HiOutlineLocationMarker className="icon"/>
-                   {dataDetail?.address}
-                  
-               </div>
-              
-            
-               
-               <div>
-                   <table className='flex'> 
-                       <ul>
-                           <li ><h4 style={{color:'black'}}>Mức giá</h4></li>
-                           <li>{dataDetail?.price}</li>
-                       </ul>
-                       <ul>
-                           <li><h4 style={{color:'black'}}>Diện tích</h4></li>
-                           <li>{dataDetail?.acreage}  <sup>2</sup></li>
-                       </ul>
-                       <ul>
-                           <li><h4 style={{color:'black'}}>Chiều dài</h4></li>
-                           <li>{dataDetail?.length}</li>
-                       </ul>
+    useEffect(() => {
+        console.log(id)
+        let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: `http://localhost:8081/api/realestates/${id}`,
+            headers: {}
+        };
 
-                       <ul>
-                           <li><h4 style={{color:'black'}}>Chiều rộng</h4></li>
-                           <li>{dataDetail?.width}</li>
-                       </ul>
+        axios.request(config)
+            .then((response) => {
+                if (response?.status === 200) {
+                    setDataDetail(response?.data)
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [])
 
-                       <ul>
-                           <li><h4 style={{color:'black'}}>Ngày đăng</h4></li>
-                           <li>{dataDetail?.dateSubmitted}</li>
-                       </ul>
-                   </table>
-               </div>
+    const [idAccuser, setIdAccuser] = React.useState('');
+    const [idcheat, setidcheat] = React.useState('');
+    const [content, setcontend] = React.useState('');
 
-               <div>
-                   <h3 style={{color:'black'}}>Thông tin mô tả</h3>
-               </div>
-               <div>
-                  {dataDetail?.decription}
+    const token = localStorage.getItem("token")
 
-               </div>
+ const getUser = () => {
+        let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: `http://localhost:8081/api/checkuser?token=${token}`,
+            headers: {}
+        };
 
-                    </div>
-                
-        </div>
-        )
+        axios.request(config)
+            .then((response) => {
+                setIdAccuser(response.data.id)
+            })
+            .catch((error) => {
+                console.log(error);
+
+            });
     }
+
+const Report = async()=>{
+    let data = JSON.stringify({
+        "accuser": {
+          "id": idAccuser
+        },
+        "cheat": {
+          "id": idcheat
+        },
+        "dateReport": Date.now(),
+        "status": "Chưa xem",
+        "content": content
+      });
+      
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:8081/api/reports',
+        headers: { 
+          'Content-Type': 'application/json'
+        },
+        data : data
+      };
+      
+      axios.request(config)
+      .then((response) => {
+        // console.log(JSON.stringify(response.data));
+        if (response.status===200) {
+            alert('Báo cáo thành công')
+            handleClose()
+        }
+       
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+}
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (<div className="container">
+        <div className='real-estate-infor'>
+            <div className="flex">
+
+                <div className="carousel-image-real-estate">
+
+                    <Carousel>
+                        <div>
+                            <img src={dataDetail?.url_img1} alt='' />
+                            {/* <p className="legend">Ảnh 1</p> */}
+                        </div>
+                        <div>
+                            <img src={dataDetail?.url_img2} alt='' />
+                            {/* <p className="legend">Ảnh 2</p> */}
+                        </div>
+                        <div>
+                            <img src={dataDetail?.url_img3} alt='' />
+                            {/* <p className="legend">Ảnh 3</p> */}
+                        </div>
+                        <div>
+                            <img src={dataDetail?.url_img4} alt='' />
+                            {/* <p className="legend">Ảnh 4</p> */}
+                        </div>
+                        <div>
+                            <img src={dataDetail?.url_img5} alt='' />
+                            {/* <p className="legend">Ảnh 5</p> */}
+                        </div>
+                        <div>
+                            <img src={dataDetail?.url_img6} alt='' />
+                            {/* <p className="legend">Ảnh 6</p> */}
+                        </div>
+                    </Carousel>
+                </div>
+                <div className="detail-real-estate">
+                    <img alt="Remy Sharp" src={dataDetail?.user.url} style={{ width: 150, height: 150, textAlign: 'center', borderRadius: 100, border: '2px solid black' }} />
+                    <br />
+                    <br />
+                    <h5 style={{ textAlign: 'center' }}>Được đăng bởi</h5>
+                    <p style={{ fontSize: 30, fontWeight: 'bold', color: 'black', textAlign: 'center' }}>{dataDetail?.user.name}</p>
+                    <a href={`/real-state-user/${dataDetail?.user.id}`}>
+                        <Button variant="outlined">Xem các sản phẩm liên quan</Button> <br />
+                    </a>
+                    <br />
+                    <ul>
+                        <li>SDT: {dataDetail?.user.phone}</li>
+                        <li>Email: {dataDetail?.user.username}</li>
+                    </ul>
+                    <Button variant="outlined">Yêu cầu liên lạc lại</Button> <br />
+                    <Button variant="outlined" color="error" style={{ marginTop: 10 }} onClick={()=>{
+                        setidcheat(dataDetail.user.id)
+                        getUser()
+                        handleClickOpen()
+                    }}>
+                        Báo cáo
+                    </Button>
+                    <Dialog
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                        style={{ width: 1500 }}
+                    >
+                        <DialogTitle id="alert-dialog-title">
+                            {"Thông tin?"}
+                        </DialogTitle>
+                        <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+
+                               
+                             
+
+                                <div style={{ marginTop: 10 }}>
+                                    <label htmlFor="">Nội dung: </label>
+                                    <textarea style={{ marginTop: 10 }}
+
+                                        name="postContent"
+                                        rows={12}
+                                        cols={65}
+                                        onChange={event => setcontend(event.target.value)}
+                                    />
+                                </div>
+                            </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                        <Button onClick={Report} >
+                               Báo cáo
+                            </Button>
+                            <Button onClick={handleClose} >
+                                Đóng
+                            </Button>
+
+
+                        </DialogActions>
+                    </Dialog>
+
+                </div>
+            </div>
+            <div>
+                <h2 style={{ color: 'black' }}>{dataDetail?.name}</h2>
+            </div>
+            <div className='flex'>
+                <HiOutlineLocationMarker className="icon" />
+                {dataDetail?.address}
+
+            </div>
+
+
+
+            <div>
+                <table className='flex'>
+                    <ul>
+                        <li ><h4 style={{ color: 'black' }}>Mức giá</h4></li>
+                        <li>{dataDetail?.price}</li>
+                    </ul>
+                    <ul>
+                        <li><h4 style={{ color: 'black' }}>Diện tích</h4></li>
+                        <li>{dataDetail?.acreage}  <sup>2</sup></li>
+                    </ul>
+                    <ul>
+                        <li><h4 style={{ color: 'black' }}>Chiều dài</h4></li>
+                        <li>{dataDetail?.length}</li>
+                    </ul>
+
+                    <ul>
+                        <li><h4 style={{ color: 'black' }}>Chiều rộng</h4></li>
+                        <li>{dataDetail?.width}</li>
+                    </ul>
+
+                    <ul>
+                        <li><h4 style={{ color: 'black' }}>Ngày đăng</h4></li>
+                        <li>{dataDetail?.dateSubmitted}</li>
+                    </ul>
+                </table>
+            </div>
+
+            <div>
+                <h3 style={{ color: 'black' }}>Thông tin mô tả</h3>
+            </div>
+            <div>
+                {dataDetail?.decription}
+
+            </div>
+
+        </div>
+
+    </div>
+    )
+}
 export default Detail
