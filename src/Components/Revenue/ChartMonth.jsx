2bcc771b6da5f6fb273cd9bba0bb5e2d0c8f9ee5
phongok <phongdevgithub666666@gmail.com>
2023-05-16 11:48:23 +0700

    update biêu đồ danh thu theo tháng

--- src/Components/Revenue/ChartMonth.jsx
+++ src/Components/Revenue/ChartMonth.jsx
@@ -1,124 +1,61 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios"
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-  } from 'chart.js';
-  import { Line } from 'react-chartjs-2';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
-  
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-  );
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
- 
+function ChartMonth() {
+  const [data, setData] = useState({});
 
+  const getData = async () => {
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:8081/api/getRevenueMonthChart',
+      headers: {}
+    };
 
-  
-
-function ChartMonth  () {
-
-    //  const data = 
-    //     {
-   
-    //         labels : ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"],
-    //         datasets: [
-    //           {
-               
-    //             data: [
-    //                 50000.0,
-    //                 0.0,
-    //                 0.0,
-    //                 400000.0,
-    //                 200000.0,
-    //                 0.0,
-    //                 0.0,
-    //                 0.0,
-    //                 150000.0,
-    //                 0.0,
-    //                 0.0,
-    //                 0.0
-    //             ],
-    //             borderColor: 'rgb(255, 99, 132)',
-    //             backgroundColor: 'rgba(255, 99, 132, 0.5)',
-               
-    //           },
-              
-    //         ],
-    //       }
-     
-    const [data, setData] = useState({})
-       
-
-    const getData = async () =>{
-        let config = {
-            method: 'get',
-            maxBodyLength: Infinity,
-            url: 'http://localhost:8081/api/getRevenueMonthChart',
-            headers: { }
-          };
-          
-          try {
-            const response = await axios.request(config);
-            setData({
-              labels: [
-                'Tháng 1',
-                'Tháng 2',
-                'Tháng 3',
-                'Tháng 4',
-                'Tháng 5',
-                'Tháng 6',
-                'Tháng 7',
-                'Tháng 8',
-                'Tháng 9',
-                'Tháng 10',
-                'Tháng 11',
-                'Tháng 12'
-              ],
-              datasets: [
-                {
-                  data: response.data,
-                  borderColor: 'rgb(255, 99, 132)',
-                  backgroundColor: 'rgba(255, 99, 132, 0.5)'
-                }
-              ]
-            });
-            console.log(data);
-          } catch (error) {
-            console.log(error);
+    try {
+      const response = await axios.request(config);
+      setData({
+        labels: [
+          'Tháng 1',
+          'Tháng 2',
+          'Tháng 3',
+          'Tháng 4',
+          'Tháng 5',
+          'Tháng 6',
+          'Tháng 7',
+          'Tháng 8',
+          'Tháng 9',
+          'Tháng 10',
+          'Tháng 11',
+          'Tháng 12'
+        ],
+        datasets: [
+          {
+            data: response.data,
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)'
           }
-        };
-   useEffect(() => {
-     getData()
-  }, [])
-
-
-
-
-   
-    return (
-        <div>
-      
-
-             
-            
-                            
-        <Line data={data}></Line>      
-                           
-            
-        </div>
-    )
+        ]
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
     }
-export default ChartMonth
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+    {data.datasets && data.labels ? <Line data={data} /> : null}
+  </div>
+  );
+}
+
+export default ChartMonth;
